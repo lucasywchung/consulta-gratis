2f69412001fb3aab2688761586895b863b34dba5
lucasywchung <78174272+lucasywchung@users.noreply.github.com>
2021-01-29 01:10:17 +0100

    Add files via upload

---
+++ minified/assets/js/index.js
@@ -0,0 +1,30 @@
+//SCROLL REVEAL
+
+//LANDING PAGE
+//Heading Masthead
+ScrollReveal().reveal('.masthead');
+ScrollReveal().reveal('.masthead-subheading', {delay:100});
+
+//POR QUE TOFU SECTION
+ScrollReveal().reveal('.page-section');
+ScrollReveal().reveal('.icon', {delay:100});
+ScrollReveal().reveal('.section-heading', {delay:100});
+ScrollReveal().reveal('.title', {delay:100});
+ScrollReveal().reveal('.title-subheading', {delay:200});
+
+//QUIENES SOMOS SECTION
+ScrollReveal().reveal('.page-section-2');
+ScrollReveal().reveal('.avatar');
+
+//CONTACTO SECTION
+ScrollReveal().reveal('.page-section-3', {delay:100});
+
+//BUTTONS
+ScrollReveal().reveal('.TofuColorBtn', {delay:100});
+ScrollReveal().reveal('.TofuColorBtn-2', {delay:100});
+ScrollReveal().reveal('.green-btn', {delay:100});
+ScrollReveal().reveal('.yellow-btn', {delay:100});
+
+/* AOS animation */
+
+AOS.init();
